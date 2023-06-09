5449630834242c6877d3f277fe94837b7f27a2cc
Clive Portman <clive@theportman.co>
2023-06-09 16:00:37 +0100

    feat: first draft of Add Food screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,10 @@
 	import '../tailwind.css';
 </script>
 
-<slot />
+<div class="h-screen max-w-sm w-full flex flex-col mx-auto p-6">
+	<div
+		class="h-full flex flex-col justify-between bg-cover bg-[url('/img/mountains-1.jpg')] bg-sky-800 bg-blend-multiply"
+	>
+		<slot />
+	</div>
+</div>
