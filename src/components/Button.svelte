cc712b508b9f6a64159aa85d2de2be2fcbfa56a4
Clive Portman <clive@theportman.co>
2023-06-12 09:06:19 +0100

    refactor: migrating code into components

---
+++ src/components/Button.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let text: string = 'Submit';
+	export let bgColour: string = 'bg-white',
+		textColour: string = 'text-sky-700';
+	export let selected: boolean, muted: boolean;
+</script>
+
+<button
+	class="block border-0 p-3 opacity-80 {selected
+		? `${bgColour} text-sky-700`
+		: `bg-sky-800 ${textColour}`}"
+	on:click={() => dispatch('click')}
+>
+	<div class="flex flex-col items-center">
+		{text}
+		<slot name="svg" />
+	</div>
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+	import Button from '$components/Button.svelte';
+	import HappyFaceIcon from '$svgs/HappyFace.svelte';
+	import NeutralFaceIcon from '$svgs/NeutralFace.svelte';
 	let proteinG = 0,
 		fatG = 0,
 		carbG = 0;
@@ -9,7 +12,7 @@
 		<p class="w-full text-white text-xl font-medium">Add a food</p>
 		<button
 			type="button"
-			class="relative -top-3 -right-3 block w-auto border-0 p-2 bg-transparent text-white opacity-80 font-medium"
+			class="relative -top-1 -right-3 block w-auto border-0 p-2 bg-transparent text-white opacity-80 font-medium"
 		>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
@@ -24,43 +27,24 @@
 		</button>
 	</div>
 	<div class="grid grid-cols-3 gap-2 px-4 mb-4 text-white font-medium">
-		<button class="block border-0 p-3 text-sky-700 bg-lime-300 opacity-80">
-			<div class="flex flex-col items-center">
-				Protein
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-					/>
-				</svg>
-			</div>
-		</button>
-		<button class="block border-0 p-3 text-sky-700 bg-yellow-300 opacity-80">
-			<div class="flex flex-col items-center">
-				Fat<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-6 h-6"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M15.182 15.182a 8.0 0 0 01-6.364 0 M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
-					/>
-				</svg>
-			</div>
-		</button>
+		<Button
+			text="Protein"
+			selected={true}
+			muted={false}
+			bgColour="bg-lime-300"
+			textColour="text-lime-300"
+		>
+			<span slot="svg"><HappyFaceIcon /></span>
+		</Button>
+		<Button
+			text="Fat"
+			selected={true}
+			muted={false}
+			bgColour="bg-yellow-300"
+			textColour="text-yellow-300"
+		>
+			<span slot="svg"><NeutralFaceIcon /></span>
+		</Button>
 		<button class="block border-0 p-3 bg-sky-700 text-red-300 opacity-60">
 			<div class="flex flex-col items-center">
 				Carb.<svg
