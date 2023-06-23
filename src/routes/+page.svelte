5ddfb002713f517cb026ccb49060d58f0143d0c6
Clive Portman <clive@theportman.co>
2023-06-23 16:12:40 +0100

    feat: ui for end users

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,79 +1,79 @@
 <script lang="ts">
-	import Button from '$components/Button.svelte';
-	import HappyFaceIcon from '$svgs/HappyFace.svelte';
-	import NeutralFaceIcon from '$svgs/NeutralFace.svelte';
-	import { each } from 'svelte/internal';
-	let proteinG = 0,
-		fatG = 0,
-		carbG = 0;
+	import { goto } from '$app/navigation';
 
-	const options = [
-		{
-			value: 1,
-			colour: 'bg-lime-50',
-		},
-		{
-			value: 2,
-			colour: 'bg-lime-100',
-		},
-		{
-			value: 3,
-			colour: 'bg-lime-200',
-		},
-		{
-			value: 5,
-			colour: 'bg-lime-300',
-		},
-		{
-			value: 8,
-			colour: 'bg-lime-400',
-		},
-		{
-			value: 13,
-			colour: 'bg-lime-500',
-		},
-		{
-			value: 21,
-			colour: 'bg-lime-600',
-		},
-		{
-			value: 34,
-			colour: 'bg-lime-700',
-		},
-	];
+	let roomNumber = '',
+		callSign = '';
+	let step = 1;
+
+	const handleContinue = () => {
+		switch (step) {
+			case 1:
+				if (validateStep1()) step = 2;
+				break;
+			case 2:
+				if (validateStep2()) goto('vote');
+				break;
+		}
+	};
+
+	const validateStep1 = () => {
+		if (roomNumber.length < 4) return false;
+		return true;
+	};
+
+	const validateStep2 = () => {
+		if (callSign.length < 4) return false;
+		return true;
+	};
 </script>
-<div class="p-3 flex flex-col justify-between h-full">
-<div class="grid gap-3">
-	{#each options as option}
-		<button type="button" class="block w-full border-0 p-3 bg-white bg-opacity-80 text-black font-medium text-xl mix-blend-screen" >
-			{option.value}
-		</button>
-	{/each}
-</div>
-<div class="">
-	<button type="button" class="block w-full border-0 p-3 bg-lime-400 bg-opacity-90 text-transparent font-medium text-xl mix-blend-screen text-black">
-		Stick
-	</button>
-</div>
-</div>
 
-<style lang="postcss">
-	.range-slider.protein::-webkit-slider-thumb {
-		-webkit-appearance: none;
-		appearance: none;
-		width: 1.6rem;
-		height: 1.6rem;
-		background: #bef264;
-		cursor: pointer;
-		border-radius: 50%;
-	}
-	.range-slider.fat::-webkit-slider-thumb {
-		-webkit-appearance: none;
-		appearance: none;
-		width: 1.6rem;
-		height: 1.6rem;
-		background: #fde047;
-		cursor: pointer;
-		border-radius: 50%;
-	}
-</style>
+<div class="p-3 flex flex-col justify-between h-full">
+	<div />
+	<div class="">
+		{#if step === 1}
+			<label class="block w-full text-lg text-white text-opacity-80">
+				What's your room number
+				<input
+					type="text"
+					class="block w-full mt-2 p-3 bg-white bg-opacity-80 text-sky-900 outline-none font-medium text-lg"
+					placeholder="e.g. 44"
+					bind:value={roomNumber}
+				/>
+			</label>
+			<p
+				class="mt-1 text-right text-xs text-white text-opacity-80"
+				class:opacity-0={step == 1 && roomNumber.length >= 4}
+			>
+				Min 6 characters
+			</p>
+		{/if}
+		{#if step === 2}
+			<label class="block w-full text-lg text-white text-opacity-80">
+				What shall we call you?
+				<input
+					type="text"
+					class="block w-full mt-2 p-3 bg-white bg-opacity-80 text-sky-900 outline-none font-medium text-lg"
+					placeholder="e.g. Punk"
+					bind:value={callSign}
+				/>
+			</label>
+			<p
+				class="mt-1 text-right text-xs text-white text-opacity-80"
+				class:opacity-0={step == 2 && callSign.length >= 4}
+			>
+				Min 6 characters
+			</p>
+		{/if}
+	</div>
+	<div class="">
+		<button
+			type="button"
+			disabled={(step == 1 && roomNumber.length < 4) || (step == 2 && callSign.length < 4)}
+			on:click={() => handleContinue()}
+			class="block w-full border-0 p-3 bg-lime-400 bg-opacity-90 text-black font-medium text-2xl mix-blend-screen"
+			class:opacity-30={(step == 1 && roomNumber.length < 4) || (step == 2 && callSign.length < 4)}
+		>
+			Continue
+		</button>
+	</div>
+</div>
---
+++ src/routes/vote/+page.svelte
@@ -0,0 +1,137 @@
+<script lang="ts">
+	let selectedValue: null | number = null;
+	let canVote = true;
+	let voted = false;
+
+	const options = [
+		{
+			value: 1,
+			colour: 'bg-lime-50'
+		},
+		{
+			value: 2,
+			colour: 'bg-lime-100'
+		},
+		{
+			value: 3,
+			colour: 'bg-lime-200'
+		},
+		{
+			value: 5,
+			colour: 'bg-lime-300'
+		},
+		{
+			value: 8,
+			colour: 'bg-lime-400'
+		},
+		{
+			value: 13,
+			colour: 'bg-lime-500'
+		},
+		{
+			value: 21,
+			colour: 'bg-lime-600'
+		},
+		{
+			value: 34,
+			colour: 'bg-lime-700'
+		}
+	];
+
+	const availableClasses = 'bg-white bg-opacity-80';
+	const selectedClasses = 'bg-lime-400 bg-opacity-80';
+	const unselectedClasses = 'bg-white bg-opacity-30';
+
+	const handleOptionToggle = (value: number) => {
+		selectedValue === value ? (selectedValue = null) : (selectedValue = value);
+	};
+
+	const handleCancelVote = () => {
+		voted = false;
+		selectedValue = null;
+	};
+</script>
+
+<div class="p-3 flex flex-col justify-between h-full">
+	<div>
+		<div class="flex justify-between items-center mb-3">
+			<p class="text-sm text-white opacity-80 text-left">
+				Room: #1234<br />Callsign: Cool Hand Clover
+			</p>
+			<button type="button" class="block p-3 -mr-3 underline text-white text-opacity-80">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div
+			class="block w-full border-0 p-3 bg-white bg-opacity-80 text-black font-medium mix-blend-screen"
+		>
+			<h1 class="text-2xl">COM-9041</h1>
+			<p>
+				Contact properties refactor that is going to have a description reaching multiple lines,
+				even as many as three.
+			</p>
+		</div>
+	</div>
+	<div class="grid grid-cols-2 gap-3">
+		{#each options as option}
+			<button
+				type="button"
+				disabled={voted}
+				on:click={() => {
+					if (canVote) handleOptionToggle(option.value);
+				}}
+				class="block w-full border-0 p-3 text-black font-medium text-xl mix-blend-screen {!selectedValue
+					? availableClasses
+					: option.value === selectedValue
+					? selectedClasses
+					: unselectedClasses}"
+			>
+				{option.value}
+			</button>
+		{/each}
+		<div class="col-span-2">
+			<p class="text-sm text-white opacity-80">
+				Voting is <span
+					class="uppercase font-medium text-lime-400"
+					class:text-lime-400={canVote}
+					class:text-red-400={!canVote}>{canVote ? 'open' : 'closed'}</span
+				>
+			</p>
+			<p class="text-sm text-white opacity-0" class:opacity-80={voted} class:opacity-0={!voted}>
+				You have voted. <button
+					type="button"
+					disabled={!voted}
+					on:click={() => handleCancelVote()}
+					class="underline">Change your mind?</button
+				>
+			</p>
+		</div>
+	</div>
+	<div class="">
+		<button
+			type="button"
+			disabled={selectedValue === null}
+			on:click={() => (voted = !voted)}
+			class="block w-full border-0 p-3 bg-lime-400 bg-opacity-90 text-black font-medium text-2xl mix-blend-screen"
+			class:opacity-30={!selectedValue || voted}
+		>
+			{#if !selectedValue}Go ahead, punk
+			{:else if voted}You made your choice
+			{:else}You sure?
+			{/if}
+		</button>
+	</div>
+</div>
