5df1473c86cde531895f25d8f008c4965ac99abf
Clive Portman <clive@theportman.co>
2023-06-23 16:24:04 +0100

    feat: adding loading indicator

--- src/routes/vote/+page.svelte
+++ src/routes/vote/+page.svelte
@@ -120,18 +120,43 @@
 			</p>
 		</div>
 	</div>
-	<div class="">
-		<button
-			type="button"
-			disabled={selectedValue === null}
-			on:click={() => (voted = !voted)}
-			class="block w-full border-0 p-3 bg-lime-400 bg-opacity-90 text-black font-medium text-2xl mix-blend-screen"
-			class:opacity-30={!selectedValue || voted}
-		>
-			{#if !selectedValue}Go ahead, punk
-			{:else if voted}You made your choice
-			{:else}You sure?
-			{/if}
-		</button>
+	<div class="h-14">
+		{#if voted}
+			<div class="flex justify-center items-center h-14">
+				<svg
+					class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<circle
+						class="opacity-25"
+						cx="12"
+						cy="12"
+						r="10"
+						stroke="currentColor"
+						stroke-width="4"
+					/>
+					<path
+						class="opacity-75"
+						fill="currentColor"
+						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+					/>
+				</svg>
+				<p class="text-sm text-white opacity-80 text-left">Waiting for the others</p>
+			</div>
+		{:else}
+			<button
+				type="button"
+				disabled={selectedValue === null}
+				on:click={() => (voted = !voted)}
+				class="block w-full border-0 p-3 bg-lime-400 bg-opacity-90 text-black font-medium text-2xl mix-blend-screen"
+				class:opacity-30={!selectedValue || voted}
+			>
+				{#if !selectedValue}Go ahead, punk
+				{:else}You sure?
+				{/if}
+			</button>
+		{/if}
 	</div>
 </div>
